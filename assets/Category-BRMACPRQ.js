fe0528b18bee4cd09f0b27801b5a1ed8f576c918
mrpolartw <mrpolartw@users.noreply.github.com>
2026-03-29 11:22:49 +0000

    deploy: ce6d55f503934ceb3e2723141b8b9a85ebcac22c

--- assets/Category-BRMACPRQ.js
+++
@@ -1,1 +0,0 @@
-import{u as s,a as o,r as n,R as r,j as c,b as l}from"./index-5KUD79lY.js";const i={"/main-food":"food","/snacks":"snacks","/health":"health","/joints":"health"};function f(){const a=s(),e=o();return n.useEffect(()=>{const t=i[e.pathname]||"all";a(`${r.PRODUCTS}?category=${t}`,{replace:!0})},[e.pathname,a]),c.jsx(l,{size:"large",fullPage:!0,label:"載入中..."})}export{f as default};
